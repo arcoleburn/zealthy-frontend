0f86ae2a94f1b3df91253a0673aa9dcbd4a7beab
Andrew Coleburn <arcoleburn@gmail.com>
2025-01-24 15:34:25 -0500

    nav

--- src/scenes/Nav/Nav.tsx
+++ src/scenes/Nav/Nav.tsx
@@ -1,30 +1,70 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import { useAppContext } from "../../shared/AppContext/AppContextProvider";
-import styles from "./Nav.module.css";
+
 const Nav = () => {
   const { setUser, setIsLoggedIn, isLoggedIn } = useAppContext();
   const navigate = useNavigate();
 
   return (
-    <nav className={styles.nav}>
-      <Link to="/">Home</Link>
-
-      <Link to="/admin">Admin</Link>
-      <Link to="/data">Data</Link>
-
-      {isLoggedIn ? (
-        <button
-          onClick={() => {
-            setUser(null);
-            setIsLoggedIn(false);
-            navigate("/");
-          }}
+    <nav>
+      <div className="flex space-evenly justify-center rounded-lg">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "underline text-white py-2 px-4"
+              : "text-white hover:text-gray-300 py-2 px-4"
+          }
         >
-          Logout
-        </button>
-      ) : (
-        <Link to="/login">Login</Link>
-      )}
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/admin"
+          className={({ isActive }) =>
+            isActive
+              ? "underline text-white py-2 px-4"
+              : "text-white hover:text-gray-300 py-2 px-4"
+          }
+        >
+          Admin
+        </NavLink>
+
+        <NavLink
+          to="/data"
+          className={({ isActive }) =>
+            isActive
+              ? "underline text-white py-2 px-4"
+              : "text-white hover:text-gray-300 py-2 px-4"
+          }
+        >
+          Data
+        </NavLink>
+
+        {isLoggedIn ? (
+          <button
+            onClick={() => {
+              setUser(null);
+              setIsLoggedIn(false);
+              navigate("/");
+            }}
+            className="text-white hover:text-gray-300 py-2 px-4"
+          >
+            Logout
+          </button>
+        ) : (
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive
+                ? "underline text-white py-2 px-4"
+                : "text-white hover:text-gray-300 py-2 px-4"
+            }
+          >
+            Login
+          </NavLink>
+        )}
+      </div>
     </nav>
   );
 };
